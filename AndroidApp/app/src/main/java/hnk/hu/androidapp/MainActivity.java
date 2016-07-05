c4e355eca43bf01656e0dc25b10b6ac6c98039d4
Nándor Holozsnyák <holi60@gmail.com>
2016-07-05 22:38:15 +0200

    minimal chat funkciók

--- AndroidApp/app/src/main/java/hnk/hu/androidapp/MainActivity.java
+++ AndroidApp/app/src/main/java/hnk/hu/androidapp/MainActivity.java
@@ -1,6 +1,5 @@
 package hnk.hu.androidapp;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
@@ -16,35 +15,104 @@ import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Presence;
-import org.jivesoftware.smack.packet.XMPPError;
+
+import java.io.IOException;
+import java.util.Properties;
 
 import static android.widget.Toast.makeText;
 
 public class MainActivity extends AppCompatActivity {
 
     String msg = "Android : ";
+    /**
+     * XMPPP Connection
+     */
     private XMPPConnection conn;
+    /**
+     * Kapcsolódás konfig.
+     */
     private ConnectionConfiguration connectionConfiguration;
+    /**
+     * Bejelentkezési név.
+     */
     private EditText loginName;
+    /**
+     * Jelszó.
+     */
     private EditText password;
+    /**
+     * Kapcsolódási tulajdonságok.
+     */
+    private Properties prop;
+
+    //======================
+    private String host;
+    private String service;
+    private int port;
+    //======================
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(msg, "The onCreate {} event");
-        connectionConfiguration = new ConnectionConfiguration("192.168.0.103", 5222, "192.168.0.103");
+
+        prop = new Properties();
+        try {
+            loadProperties();
+        } catch (IOException e) {
+            Log.d("Error", "error!");
+        }
+
+        createXMPPConnectionConfiguration();
+
+        checkForStrictMode();
+
+        makeConnection();
+    }
+
+    private void createXMPPConnectionConfiguration() {
+        connectionConfiguration = new ConnectionConfiguration(host, port, service);
         connectionConfiguration.setSecurityMode(ConnectionConfiguration.SecurityMode.disabled);
         connectionConfiguration.setSASLAuthenticationEnabled(false);
         conn = new XMPPConnection(connectionConfiguration);
         SASLAuthentication.supportSASLMechanism("DIGEST-MD5", 0);
+    }
 
+    private void checkForStrictMode() {
         if (android.os.Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
+    }
 
+    private void makeConnection() {
+        if (!conn.isConnected()) {
+            try {
+                conn.connect();
+                makeText(this, "Connected to OpenFire server.", Toast.LENGTH_LONG).show();
+                findViewById(R.id.send).setEnabled(true);
+                findViewById(R.id.connect).setEnabled(true);
+            } catch (XMPPException | VerifyError e) {
+                makeText(this, "Could not establish connection to OpenFire server.", Toast.LENGTH_LONG).show();
+                findViewById(R.id.connect).setEnabled(false);
+                Log.d(msg, e.getMessage());
+            }
+        }
+    }
 
+    /**
+     * Szerver tulajdonságok betöltése fájlból.
+     *
+     * @throws IOException
+     */
+    private void loadProperties() throws IOException {
+
+        prop.load(getBaseContext().getAssets().open("server.properties"));
+        host = prop.getProperty("server.host");
+        service = prop.getProperty("service.name");
+        port = Integer.parseInt(prop.getProperty("port"));
+        Log.d(msg, "Server configuriation properties set.\n " + host + ":" + port + " with service " + service);
     }
 
     /**
@@ -92,6 +160,11 @@ public class MainActivity extends AppCompatActivity {
         Log.d(msg, "The onDestroy() event");
     }
 
+    /**
+     * Küldés gomb
+     *
+     * @param view
+     */
     public void buttonClick(View view) {
         Button button = (Button) view;
         Log.d(msg, "is connected:" + conn.isConnected());
@@ -107,24 +180,29 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Kapcsolat létrehozása
+     *
+     * @param view
+     */
     public void makeConnection(View view) {
         Button button = (Button) view;
         loginName = (EditText) findViewById(R.id.loginName);
         password = (EditText) findViewById(R.id.password);
         Log.d(msg, "is connected:" + conn.isConnected());
 
-        if (!conn.isConnected()) {
-            try {
-                conn.connect();
-                makeText(this, "Connected to OpenFire server.", Toast.LENGTH_LONG).show();
-            } catch (XMPPException | VerifyError e) {
-                makeText(this, "Could not establish connection to OpenFire server.", Toast.LENGTH_LONG).show();
-            }
-        }
+        makeConnection();
+        doLogin(view);
+    }
 
+    /**
+     * Bejelentkezés
+     *
+     * @param view
+     */
+    private void doLogin(View view) {
         if (!conn.isAuthenticated())
             try {
-
                 Log.d(msg, "Username:" + loginName.getText().toString());
                 Log.d(msg, "pass:" + password.getText().toString());
                 conn.login(loginName.getText().toString(), password.getText().toString());
@@ -132,23 +210,22 @@ public class MainActivity extends AppCompatActivity {
                 Presence presence = new Presence(Presence.Type.available);
                 conn.sendPacket(presence);
                 view.setEnabled(false);
-
             } catch (XMPPException | VerifyError e) {
                 makeText(this, "Something wrong happened.", Toast.LENGTH_LONG).show();
                 Log.d(msg, e.getMessage());
             }
     }
 
+    /**
+     * Kijelentkezés gomb akciója.
+     *
+     * @param view
+     */
     public void disconnect(View view) {
         if (conn.isAuthenticated()) {
-            Presence presence = new Presence(Presence.Type.unavailable);
-            conn.sendPacket(presence);
             conn.disconnect();
             makeText(this, "Disconnected successfuly.", Toast.LENGTH_LONG).show();
             findViewById(R.id.connect).setEnabled(true);
         }
     }
 }
-
-
-
